cd624b62663eb9b8c61dd60dbb4eaa069c9e8b49
jcc03 <jcc03@live.com>
2024-12-09 12:45:28 -0600

    4

--- 6pqLr/keymap.c
+++ 6pqLr/keymap.c
@@ -231,3 +231,11 @@ tap_dance_action_t tap_dance_actions[] = {
         [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_0_finished, dance_0_reset),
         [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_1_finished, dance_1_reset),
 };
+
+// Custom QMK here
+const key_override_t delete_key_override = 
+    ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+const key_override_t *key_overrides[] = {
+	&delete_key_override
+};
