c01ec28cd71da6bcadef39e9888f7923840bcfb2
jcc03 <jcc03@live.com>
2024-12-09 13:28:09 -0600

    6th

--- 6pqLr/keymap.c
+++ 6pqLr/keymap.c
@@ -232,6 +232,8 @@ tap_dance_action_t tap_dance_actions[] = {
         [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_1_finished, dance_1_reset),
 };
 
+#include QMK_KEYBOARD_H
+
 // Define the custom key override
 const key_override_t delete_key_override = 
     ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
