b1adcde620d16196c7b8de59c4cf34d943f903a6
jcc03 <jcc03@live.com>
2024-12-10 13:43:26 -0600

    colon

--- 6pqLr/features/achordion.c
+++ 6pqLr/features/achordion.c
@@ -349,9 +349,10 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
 }
 
 // By default, the timeout is 1000 ms for all keys.
-__attribute__((weak)) uint16_t achordion_timeout(uint16_t tap_hold_keycode) {switch (tap_hold_keycode) {
+__attribute__((weak)) uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
+  switch (tap_hold_keycode) {
     case MOD_LCTL:
-    case LT(1,KC_SPACE)
+    case LT(1,KC_SPACE):
       return 0;  // Bypass Achordion for these keys.
   }
   return 1000;
