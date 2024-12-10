9e0a543cd621e990da07edf031f09a25c1e9a2a3
jcc03 <jcc03@live.com>
2024-12-10 13:11:25 -0600

    double

--- 6pqLr/features/achordion.c
+++ 6pqLr/features/achordion.c
@@ -342,7 +342,7 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
       break;
 
     case LT(1,KC_SPACE):  // IJKL
-      if (other_keycode == KC_I || other_keycode == KC_J || other_keycode == KC_K || other_keycode == KC_L ||) { return true; }
+      if (other_keycode == KC_I || other_keycode == KC_J || other_keycode == KC_K || other_keycode == KC_L) { return true; }
       break;
     }                                     
   return achordion_opposite_hands(tap_hold_record, other_record);
@@ -350,7 +350,7 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
 
 // By default, the timeout is 1000 ms for all keys.
 __attribute__((weak)) uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
-  
+  return 1000;
 }
 
 // By default, Shift and Ctrl mods are eager, and Alt and GUI are not.
