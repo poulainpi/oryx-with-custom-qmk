deb2bbbbe0a8527dd50aaffddaa720da4e712b35
Scott <scottrych@gmail.com>
2025-01-08 11:38:25 -0500

    Update keymap.c

--- EoEv5/keymap.c
+++ EoEv5/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "features/achordion.h"
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
 
@@ -144,6 +145,7 @@ bool rgb_matrix_indicators_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_achordion(keycode, record)) { return false; }
   switch (keycode) {
 
     case RGB_SLD:
