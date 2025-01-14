b9d3b25db554196a02699b5402aab48c94bb2781
jcc03 <jcc03@live.com>
2025-01-14 16:27:35 -0600

    minor tweak

--- 6pqLr/features/achordion.c
+++ 6pqLr/features/achordion.c
@@ -386,10 +386,7 @@ __attribute__((weak)) bool achordion_streak_continue(uint16_t keycode) {
 
 uint16_t achordion_streak_chord_timeout(
     uint16_t tap_hold_keycode, uint16_t next_keycode) {
-  if (IS_QK_LAYER_TAP(tap_hold_keycode)) {
-    return 0;  // Disable streak detection on layer-tap keys.
-  }
-
+ 
   // Otherwise, tap_hold_keycode is a mod-tap key.
   uint8_t mod = mod_config(QK_MOD_TAP_GET_MODS(tap_hold_keycode));
   if ((mod & (MOD_LSFT | MOD_RSFT)) != 0) {
