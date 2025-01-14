3255facc127bf13b8649b1d2b4f85b1d051e8c51
jcc03 <jcc03@live.com>
2025-01-14 10:24:17 -0600

    Streak

--- 6pqLr/config.h
+++ 6pqLr/config.h
@@ -22,7 +22,7 @@
 //
 #define MT(mod, kc) (QK_MOD_TAP | (((mod)&0x1F) << 8) | ((kc)&0xFF))
 #define LT(layer, kc) (QK_LAYER_TAP | (((layer)&0xF) << 8) | ((kc)&0xFF))
-//#define ACHORDION_STREAK
+#define ACHORDION_STREAK
 
 #undef ENABLE_RGB_MATRIX_ALPHAS_MODS
 #undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
