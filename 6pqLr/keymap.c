6ade7679b6688a670635c7140592eca485e61684
jcc03 <jcc03@live.com>
2025-02-03 08:47:41 -0600

    hmm

--- 6pqLr/keymap.c
+++ 6pqLr/keymap.c
@@ -252,6 +252,6 @@ const key_override_t parenthesis2_override = ko_make_basic(MOD_MASK_SHIFT, KC_RB
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
-	&delete_key_override,
+	//&delete_key_override,
 	NULL // Null terminate the array of overrides! 
 };
