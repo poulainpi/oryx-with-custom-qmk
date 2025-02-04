e39779ce673d6e37da162bb64841b168a3fa0bb5
jcc03 <jcc03@live.com>
2025-02-04 13:12:44 -0600

    Debounc

--- 6pqLr/rules.mk
+++ 6pqLr/rules.mk
@@ -1,5 +1,6 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+DEBOUNCE_TYPE = sym_eager_pk
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 MOUSEKEY_ENABLE = no
