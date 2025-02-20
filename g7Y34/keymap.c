d86ec5a955fc2f8cda5ffb7360720ef8d32e122b
Matthew Britt <msbritt@gmail.com>
2025-02-20 10:34:02 -0500

    Add Delete when pressing Shift + Backspace

--- g7Y34/keymap.c
+++ g7Y34/keymap.c
@@ -159,3 +159,14 @@ void dance_0_reset(tap_dance_state_t *state, void *user_data) {
 tap_dance_action_t tap_dance_actions[] = {
         [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
 };
+
+
+
+// Custom QMK here
+const key_override_t delete_key_override = 
+    ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&delete_key_override,
+	NULL
+};
