b57378077a3e04c53e5d486879842c2393aae14d
MarkusSchencker <35331193+MarkusSchencker@users.noreply.github.com>
2025-04-19 01:01:27 +0200

    Update keymap.c

--- vrMEr/keymap.c
+++ vrMEr/keymap.c
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const key_override_t dot_comma_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_COMM);
-const key_override_t quote_doublequote_override = ko_make_basic(MOD_MASK_SHIFT, DE_DQOT, DE_QOT);
+const key_override_t quote_doublequote_override = ko_make_basic(MOD_MASK_SHIFT, DE_DQOT, DE_QUOT);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
     &dot_comma_override,
