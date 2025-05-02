c4ab63c7cfe165051ef8ffd039ea334ed4688b4b
bilsonwilcox <64692449+bilsonwilcox@users.noreply.github.com>
2025-05-02 16:35:05 -0400

    Update keymap.c

--- WlgEz/keymap.c
+++ WlgEz/keymap.c
@@ -56,7 +56,7 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
   'L', 'L', 'L', 'L', 'L', 'L',             'R', 'R', 'R', 'R', 'R', 'R',
   'L', 'L', 'L', 'L', 'L', 'L',             'R', 'R', 'R', 'R', 'R', 'R',
   'L', 'L', 'L', 'L', 'L', 'L',             'R', 'R', 'R', 'R', 'R', 'R',
-                           'L', 'L',   'R', 'R'
+                           '*', '*',   '*', '*'
   );
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
