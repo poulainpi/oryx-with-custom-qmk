7b3bae87bff2307f324ff76ed6e788c3ac76c2ce
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-05-16 21:43:16 -0700

    Update keymap.c

--- 6JP4n/keymap.c
+++ 6JP4n/keymap.c
@@ -966,3 +966,19 @@ tap_dance_action_t tap_dance_actions[] = {
         [DANCE_19] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_19, dance_19_finished, dance_19_reset),
         [DANCE_20] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_20, dance_20_finished, dance_20_reset),
 };
+
+const uint16_t PROGMEM combo0[] = { KC_Y, KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo1[] = { KC_I, KC_O, KC_P, COMBO_END};
+const uint16_t PROGMEM combo2[] = { KC_U, KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM combo3[] = { MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_RALT, KC_L), COMBO_END};
+const uint16_t PROGMEM combo4[] = { MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G, COMBO_END};
+const uint16_t PROGMEM combo5[] = { MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(combo0, KC_HOME),
+    COMBO(combo1, KC_END),
+    COMBO(combo2, LCTL(KC_LEFT_SHIFT)),
+    COMBO(combo3, KC_ENTER),
+    COMBO(combo4, CW_TOGG),
+    COMBO(combo5, KC_CAPS),
+};
