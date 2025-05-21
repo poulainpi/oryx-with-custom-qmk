8fd3c51f31df4fdce8c2dbe024d55f2a7c87ff98
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-05-21 02:15:23 +0000

    Changed layout swithcihg from ctrl to shft+ctrl

--- 6JP4n/keymap.c
+++ 6JP4n/keymap.c
@@ -109,23 +109,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case LANG_SWITCH_COMBO_ACTION:
-      if (record->event.pressed) {
-          uint8_t current_highest_layer = get_highest_layer(layer_state);
-  
-          if (current_highest_layer == QMK_LAYER_ENG) {
-              layer_move(QMK_LAYER_RUS);
-              register_code(KC_LCTL);
-              register_code(KC_2);
-              unregister_code(KC_2);
-              unregister_code(KC_LCTL);
-          } else {
-              layer_move(QMK_LAYER_ENG);
-              register_code(KC_LCTL);
-              register_code(KC_1);
-              unregister_code(KC_1);
-              unregister_code(KC_LCTL);
-          }
-      }
+
+    if (record->event.pressed) {
+    	uint8_t current_highest_layer = get_highest_layer(layer_state);
+
+	if (current_highest_layer == QMK_LAYER_ENG) {
+	    layer_move(QMK_LAYER_RUS);
+	    SEND_STRING(SS_LCTL(SS_LSFT("2")));
+	} else {
+	    layer_move(QMK_LAYER_ENG);
+	    SEND_STRING(SS_LCTL(SS_LSFT("1")));
+	}
+    }
+
     return false;
     case ST_MACRO_0:
     if (record->event.pressed) {
