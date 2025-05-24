3f741683c83a3f0e409892f4449bbcc2dc833210
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-05-24 01:39:09 +0000

    Added modules

---
+++ 6JP4n/modules/combos.c
@@ -0,0 +1,15 @@
+const uint16_t PROGMEM combo0[] = { KC_Y, KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo1[] = { KC_I, KC_O, KC_P, COMBO_END};
+const uint16_t PROGMEM combo2[] = { KC_U, KC_I, KC_O, COMBO_END};
+const uint16_t PROGMEM combo3[] = { MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_RALT, KC_L), COMBO_END};
+const uint16_t PROGMEM combo4[] = { MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G, COMBO_END};
+const uint16_t PROGMEM combo5[] = { MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), COMBO_END};
+
+combo_t key_combos[] = {
+    COMBO(combo0, KC_HOME),
+    COMBO(combo1, KC_END),
+    COMBO(combo2, LANG_SWITCH_COMBO_ACTION),
+    COMBO(combo3, KC_ENTER),
+    COMBO(combo4, CW_TOGG),
+    COMBO(combo5, KC_CAPS),
+};
