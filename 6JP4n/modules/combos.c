142e047239c6cb3fdf31e670b6693b8141468ada
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-05-24 03:45:37 -0700

    Update combos.c

--- 6JP4n/modules/combos.c
+++ 6JP4n/modules/combos.c
@@ -16,7 +16,7 @@ combo_t key_combos[] = {
     COMBO(combo3, KC_ENTER),
     COMBO(combo4, CW_TOGG),
     COMBO(combo5, KC_CAPS),
-    COMBO(combo6, KC_PC_COPY),
-    COMBO(combo7, KC_PC_PASTE),
-    COMBO(combo8, KC_PC_CUT),
+    COMBO(combo6, KC_COPY),
+    COMBO(combo7, KC_PASTE),
+    COMBO(combo8, KC_CUT),
 };
