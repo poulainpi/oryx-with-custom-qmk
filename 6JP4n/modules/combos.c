2989d316dc54dba76c567a8e0245a608575b4569
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-05-24 03:30:08 -0700

    Update combos.c

--- 6JP4n/modules/combos.c
+++ 6JP4n/modules/combos.c
@@ -4,8 +4,11 @@ const uint16_t PROGMEM combo2[] = { KC_U, KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM combo3[] = { MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_RALT, KC_L), COMBO_END};
 const uint16_t PROGMEM combo4[] = { MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G, COMBO_END};
 const uint16_t PROGMEM combo5[] = { MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), COMBO_END};
-const uint16_t PROGMEM combo2[] = { KC_W, KC_E, KC_R, COMBO_END};
-    
+const uint16_t PROGMEM combo6[] = { KC_W, KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM combo7[] = { MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), COMBO_END};
+const uint16_t PROGMEM combo8[] = { KC_X, KC_C, KC_V, COMBO_END};
+
+
 combo_t key_combos[] = {
     COMBO(combo0, KC_HOME),
     COMBO(combo1, KC_END),
@@ -14,4 +17,6 @@ combo_t key_combos[] = {
     COMBO(combo4, CW_TOGG),
     COMBO(combo5, KC_CAPS),
     COMBO(combo6, KC_PC_COPY),
+    COMBO(combo7, KC_PC_PASTE),
+    COMBO(combo8, KC_PC_CUT),
 };
