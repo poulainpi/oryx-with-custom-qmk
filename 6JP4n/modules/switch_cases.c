6fe1af43232b8d5a4090245cd3c2c552d0209992
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-05-25 00:14:28 -0700

    Update switch_cases.c

--- 6JP4n/modules/switch_cases.c
+++ 6JP4n/modules/switch_cases.c
@@ -11,5 +11,21 @@
 	    SEND_STRING(SS_LCTL(SS_LSFT("1")));
 	}
     }
+    case OS_AWARE_COPY:
+      if (record->event.pressed) {
+#if defined(OS_DETECTION_ENABLE)
+        os_variant_t host_os = detected_host_os();
+        if (host_os == OS_MACOS |
+| host_os == OS_IOS) {
+          SEND_STRING(SS_LGUI("c")); // Cmd+C для macOS/iOS
+        } else {
+          SEND_STRING(SS_LCTL("c")); // Ctrl+C для Windows/Linux/других
+        }
+#else
+        // Фоллбэк, если OS_DETECTION_ENABLE не включен (например, всегда Ctrl+C)
+        SEND_STRING(SS_LCTL("c"));
+#endif
+      }
+      return false; // Важно, чтобы QMK не обрабатывал этот кейкод дальше
 
     return false;
