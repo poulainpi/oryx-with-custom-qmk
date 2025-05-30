81755426262f3cbcce0247710de9878910c72021
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-05-29 22:51:24 -0700

    Update combos.c

--- 6JP4n/modules/combos.c
+++ 6JP4n/modules/combos.c
@@ -1,6 +1,6 @@
 const uint16_t PROGMEM combo0[] = { KC_Y, LT(7, KC_U), KC_I, COMBO_END};
 const uint16_t PROGMEM combo1[] = { KC_I, KC_O, KC_P, COMBO_END};
-const uint16_t PROGMEM combo2[] = { MT(7, KC_U), KC_I, KC_O,, COMBO_END};
+const uint16_t PROGMEM combo2[] = { MT(7, KC_U), KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM combo3[] = { MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_RALT, KC_L), COMBO_END};
 const uint16_t PROGMEM combo4[] = { MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), LT(7, KC_G), COMBO_END};
 const uint16_t PROGMEM combo5[] = { MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), COMBO_END};
