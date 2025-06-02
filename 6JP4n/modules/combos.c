9b9bf5c642504460df6bdab4b60aba3301f174c3
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-06-02 12:45:48 -0700

    Update combos.c

--- 6JP4n/modules/combos.c
+++ 6JP4n/modules/combos.c
@@ -4,9 +4,9 @@ const uint16_t PROGMEM combo1[]     = { KC_I, KC_O, KC_P, COMBO_END};
 const uint16_t PROGMEM combo2_eng[] = { KC_U, KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM combo2_rus[] = { RU_GHE,         RU_SHA,         RU_SHCH, COMBO_END};
 const uint16_t PROGMEM combo3[] = { MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_RALT, KC_L), COMBO_END};
-const uint16_t PROGMEM combo4_eng[] = { MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), LT(7, KC_G), COMBO_END};
-const uint16_t PROGMEM combo4_rus[] = { MT(MOD_LSFT, RU_VE), MT(MOD_LCTL, RU_A), LT(8, RU_PE), COMBO_END};
-const uint16_t PROGMEM combo5[] = { MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), COMBO_END};
+const uint16_t PROGMEM combo4[] = { MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), COMBO_END};
+const uint16_t PROGMEM combo5_eng[] = { MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), LT(7, KC_G), COMBO_END};
+const uint16_t PROGMEM combo5_rus[] = { MT(MOD_LSFT, RU_VE), MT(MOD_LCTL, RU_A), LT(8, RU_PE), COMBO_END};
 const uint16_t PROGMEM combo6_eng[] = { LT(9, KC_W), KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM combo6_rus[] = { LT(10, RU_TSE), RU_U, RU_KA, COMBO_END};
 const uint16_t PROGMEM combo7[] = { MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), COMBO_END};
@@ -24,9 +24,9 @@ combo_t key_combos[] = {
     COMBO(combo2_eng, LANG_SWITCH_COMBO_ACTION),
     COMBO(combo2_rus, LANG_SWITCH_COMBO_ACTION),
     COMBO(combo3, KC_ENTER),
-    COMBO(combo4_eng, CW_TOGG),
-    COMBO(combo4_rus, CW_TOGG),
-    COMBO(combo5, KC_CAPS),
+    COMBO(combo4, KC_CAPS),
+    COMBO(combo5_eng, CW_TOGG),
+    COMBO(combo5_rus, CW_TOGG),
     COMBO(combo6_eng, OS_AWARE_COPY),
     COMBO(combo6_rus, OS_AWARE_COPY),
     COMBO(combo7, OS_AWARE_PASTE),
