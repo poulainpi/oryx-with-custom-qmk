15914a6dc53acf9abe3d735f336b9c99124acb02
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-06-02 00:45:47 -0700

    Update combos.c

--- 6JP4n/modules/combos.c
+++ 6JP4n/modules/combos.c
@@ -7,13 +7,13 @@ const uint16_t PROGMEM combo3[] = { MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(M
 const uint16_t PROGMEM combo4_eng[] = { MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), LT(7, KC_G), COMBO_END};
 const uint16_t PROGMEM combo4_rus[] = { MT(MOD_LSFT, RU_VE), MT(MOD_LCTL, RU_A), LT(8, RU_PE), COMBO_END};
 const uint16_t PROGMEM combo5[] = { MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), COMBO_END};
-const uint16_t PROGMEM combo6_eng[] = { LT(10, KC_W), KC_E, KC_R, COMBO_END};
-const uint16_t PROGMEM combo6_rus[] = { LT(11, RU_TSE), RU_U, RU_KA, COMBO_END};
+const uint16_t PROGMEM combo6_eng[] = { LT(9, KC_W), KC_E, KC_R, COMBO_END};
+const uint16_t PROGMEM combo6_rus[] = { LT(10, RU_TSE), RU_U, RU_KA, COMBO_END};
 const uint16_t PROGMEM combo7[] = { MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), COMBO_END};
 const uint16_t PROGMEM combo8[] = { KC_X, KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM combo9_eng[] = { KC_N, KC_M, KC_NO, COMBO_END};
+const uint16_t PROGMEM combo9_eng[] = { KC_N, KC_M, KC_F22, COMBO_END};
 const uint16_t PROGMEM combo9_rus[] = { RU_TE, RU_SOFT, RU_BE, COMBO_END};
-const uint16_t PROGMEM combo10_eng[] = { KC_NO, KC_TRANSPARENT, KC_NO, COMBO_END};
+const uint16_t PROGMEM combo10_eng[] = { KC_F22, KC_F23, KC_F24, COMBO_END};
 const uint16_t PROGMEM combo10_rus[] = { RU_BE, RU_YU, RU_YO, COMBO_END};
 
 
