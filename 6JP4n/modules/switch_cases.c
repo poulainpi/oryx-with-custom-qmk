8259b7de067aa9bb85ccee7e4b3782ecb3816782
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-06-17 03:23:14 -0700

    Update switch_cases.c

--- 6JP4n/modules/switch_cases.c
+++ 6JP4n/modules/switch_cases.c
@@ -93,6 +93,7 @@
         case OS_AWARE_VOICE:
             if (record->event.pressed) {
 #if defined(OS_DETECTION_ENABLE)
+		os_variant_t host_os = detected_host_os();
                 if (host_os == OS_MACOS || host_os == OS_IOS) {
                     tap_code16(MAC_SIRI);
                 } else if (host_os == OS_WINDOWS) {
