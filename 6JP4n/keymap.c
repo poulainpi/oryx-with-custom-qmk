ac9b38ce49cfb436ab96d02612565f37c0eead21
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-07-04 15:43:14 -0700

    Update keymap.c

--- 6JP4n/keymap.c
+++ 6JP4n/keymap.c
@@ -50,6 +50,10 @@ enum custom_keycodes {
   ST_MACRO_35,
   ST_MACRO_36,
   MAC_SIRI,
+  OS_SHFT,
+  OS_CTRL,
+  OS_ALT,
+  OS_CMD,
   #include "./modules/custom_keycodes.c"
 };
 
@@ -236,7 +240,57 @@ bool rgb_matrix_indicators_user(void) {
 }
 
 
+bool is_oneshot_cancel_key(uint16_t keycode) {
+    switch (keycode) {
+    case LA_SYM:
+    case LA_NAV:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool is_oneshot_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+    case LA_SYM:
+    case LA_NAV:
+    case KC_LSFT:
+    case OS_SHFT:
+    case OS_CTRL:
+    case OS_ALT:
+    case OS_CMD:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool sw_win_active = false;
+bool sw_tab_active = false;
+
+oneshot_state os_shft_state = os_up_unqueued;
+oneshot_state os_ctrl_state = os_up_unqueued;
+oneshot_state os_alt_state = os_up_unqueued;
+oneshot_state os_cmd_state = os_up_unqueued;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    update_oneshot(
+        &os_shft_state, KC_LSFT, OS_SHFT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_ctrl_state, KC_LCTL, OS_CTRL,
+        keycode, record
+    );
+    update_oneshot(
+        &os_alt_state, KC_LALT, OS_ALT,
+        keycode, record
+    );
+    update_oneshot(
+        &os_cmd_state, KC_LCMD, OS_CMD,
+        keycode, record
+    );
+  
   switch (keycode) {
     #include "./modules/switch_cases.c"
     case ST_MACRO_0:
