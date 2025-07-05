6ac200fffdcc2b9e8fa8e787d0895c371da70811
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-07-04 17:00:20 -0700

    Update define.c

--- 6JP4n/modules/define.c
+++ 6JP4n/modules/define.c
@@ -1,6 +1,9 @@
 #define QMK_LAYER_ENG 0
 #define QMK_LAYER_RUS 1
 #define TAPPING_TERM 200
+#include "modules/oneshot.h"
+#include "modules/swapper.h"
+
 #if defined(OS_DETECTION_ENABLE)
 #include "os_detection.h"
 #define OS_DETECTION_DEBOUNCE 250
