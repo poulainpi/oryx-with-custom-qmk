bf6aebfad67f9cc14938d1c849d4c2c23d19dc06
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-07-05 23:54:56 -0700

    test

--- 6JP4n/keymap.c
+++ 6JP4n/keymap.c
@@ -239,36 +239,36 @@ bool rgb_matrix_indicators_user(void) {
   return true;
 }
 
-//static char hex_digit(uint8_t v) {
-//    return v < 10 ? '0' + v : 'A' + (v - 10);
-//}
+static char hex_digit(uint8_t v) {
+   return v < 10 ? '0' + v : 'A' + (v - 10);
+}
 
-//// Печатает 16-битное значение в виде 4-х HEX цифр
-//static void send_hex16(uint16_t v) {
-//    char buf[5] = {0};
-//    for (int i = 0; i < 4; i++) {
-//        buf[i] = hex_digit((v >> ((3 - i)*4)) & 0xF);
-//    }
-//    SEND_STRING(buf);
-//}
+// Печатает 16-битное значение в виде 4-х HEX цифр
+static void send_hex16(uint16_t v) {
+   char buf[5] = {0};
+   for (int i = 0; i < 4; i++) {
+       buf[i] = hex_digit((v >> ((3 - i)*4)) & 0xF);
+   }
+   SEND_STRING(buf);
+}
 
-//// Печатает 8-битное значение (0–255) в виде двух HEX цифр
-//static void send_hex8(uint8_t v) {
-//    char buf[3] = { hex_digit((v>>4)&0xF), hex_digit(v&0xF), 0 };
-//    SEND_STRING(buf);
-//}
+// Печатает 8-битное значение (0–255) в виде двух HEX цифр
+static void send_hex8(uint8_t v) {
+   char buf[3] = { hex_digit((v>>4)&0xF), hex_digit(v&0xF), 0 };
+   SEND_STRING(buf);
+}
 
 
 bool is_oneshot_cancel_key(uint16_t keycode, keyrecord_t *record) {
 
 
-    //if (record->event.pressed) {
-    //    SEND_STRING("RELEASE code=0x");
-    //    send_hex16(keycode);
-   //     SEND_STRING(" tc=");
-    //    send_hex8(record->tap.count);
-    //    SEND_STRING("\n");
-    //}
+    if (record->event.pressed) {
+       SEND_STRING("RELEASE code=0x");
+       send_hex16(keycode);
+       SEND_STRING(" tc=");
+       send_hex8(record->tap.count);
+       SEND_STRING("\n");
+    }
 
 
 
