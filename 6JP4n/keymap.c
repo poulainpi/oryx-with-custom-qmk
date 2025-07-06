bc5b47e029d7fd64384206936adaaed459c7605a
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-07-05 21:31:36 -0700

    Update keymap.c

--- 6JP4n/keymap.c
+++ 6JP4n/keymap.c
@@ -261,9 +261,7 @@ static void send_hex8(uint8_t v) {
 
 bool is_oneshot_cancel_key(uint16_t keycode, keyrecord_t *record) {
 
-  if (record->event.pressed) {
-    SEND_STRING("[DBG-ENTRY]\n");
-}
+
     if (!record->event.pressed) {
         SEND_STRING("RELEASE code=0x");
         send_hex16(keycode);
@@ -281,7 +279,9 @@ bool is_oneshot_cancel_key(uint16_t keycode, keyrecord_t *record) {
     if ((keycode & QK_LAYER_TAP) == QK_LAYER_TAP) {
         uint8_t layer = (keycode >> 8) & 0xFF;
         uint8_t kc    =  keycode        & 0xFF;
-        if (record->tap.count > 0) return false;
+  if (record->event.pressed) {
+    SEND_STRING("[DBG-ENTRY]\n");
+}        if (record->tap.count > 0) return false;
         if ((kc == KC_DELETE || kc == KC_SPACE) && layer_state_is(layer)) {
             return true;
         }
