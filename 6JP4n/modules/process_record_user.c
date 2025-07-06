78efa1b4e6979239bf44f3113e27525ba89ed33f
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-07-06 09:57:58 -0700

    test

--- 6JP4n/modules/process_record_user.c
+++ 6JP4n/modules/process_record_user.c
@@ -1,16 +1,26 @@
-    update_oneshot(
-        &os_shft_state, KC_LSFT, OS_SHFT,
-        keycode, record
-    );
-    update_oneshot(
-        &os_ctrl_state, KC_LCTL, OS_CTRL,
-        keycode, record
-    );
-    update_oneshot(
-        &os_alt_state, KC_LALT, OS_ALT,
-        keycode, record
-    );
-    update_oneshot(
-        &os_cmd_state, KC_LCMD, OS_CMD,
-        keycode, record
-    );
+
+    if (
+        !update_oneshot(&os_shft_state, KC_LSFT, OS_SHFT, keycode, record)
+    || !update_oneshot(&os_ctrl_state, KC_LCTL, OS_CTRL, keycode, record)
+    || !update_oneshot(&os_alt_state,  KC_LALT, OS_ALT,  keycode, record)
+    || !update_oneshot(&os_cmd_state,  KC_LCMD, OS_CMD,  keycode, record)
+    ) {
+        return false;
+    }
+
+    // update_oneshot(
+    //     &os_shft_state, KC_LSFT, OS_SHFT,
+    //     keycode, record
+    // );
+    // update_oneshot(
+    //     &os_ctrl_state, KC_LCTL, OS_CTRL,
+    //     keycode, record
+    // );
+    // update_oneshot(
+    //     &os_alt_state, KC_LALT, OS_ALT,
+    //     keycode, record
+    // );
+    // update_oneshot(
+    //     &os_cmd_state, KC_LCMD, OS_CMD,
+    //     keycode, record
+    // );
