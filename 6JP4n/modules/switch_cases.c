e8dd941d00edd66504ff0f9078f2605d3fb88286
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-07-06 15:21:07 -0700

    Update switch_cases.c

--- 6JP4n/modules/switch_cases.c
+++ 6JP4n/modules/switch_cases.c
@@ -1,6 +1,8 @@
     case LANG_SWITCH_COMBO_ACTION:
-      if (record->event.pressed) {
+
+    if (record->event.pressed) {
         uint8_t current_highest_layer = get_highest_layer(layer_state);
+
         if (current_highest_layer == QMK_LAYER_ENG) {
             layer_move(QMK_LAYER_RUS);
             SEND_STRING(SS_LCTL(SS_LSFT("2")));
@@ -13,74 +15,93 @@
 
     case OS_AWARE_COPY:
       if (record->event.pressed) {
+#if defined(OS_DETECTION_ENABLE)
         os_variant_t host_os = detected_host_os();
         if (host_os == OS_MACOS || host_os == OS_IOS) {
           SEND_STRING(SS_LGUI("c"));
         } else {
           SEND_STRING(SS_LCTL("c"));
         }
+#else
         SEND_STRING(SS_LCTL("c"));
+#endif
       }
-      return false; 
+      return false;
 
     case OS_AWARE_PASTE:
       if (record->event.pressed) {
+#if defined(OS_DETECTION_ENABLE)
         os_variant_t host_os = detected_host_os();
         if (host_os == OS_MACOS || host_os == OS_IOS) {
           SEND_STRING(SS_LGUI("v"));
         } else {
           SEND_STRING(SS_LCTL("v"));
         }
+#else
         SEND_STRING(SS_LCTL("v"));
+#endif
       }
       return false;
+    case OS_AWARE_CUT:
 
-    case OS_AWARE_CUT:	    
       if (record->event.pressed) {
+#if defined(OS_DETECTION_ENABLE)
         os_variant_t host_os = detected_host_os();
         if (host_os == OS_MACOS || host_os == OS_IOS) {
           SEND_STRING(SS_LGUI("x"));
         } else {
           SEND_STRING(SS_LCTL("x"));
         }
+#else
         SEND_STRING(SS_LCTL("x"));
+#endif
       }
-      return false; 
+      return false;
 
-    case OS_AWARE_UNDO:	    
+    case OS_AWARE_UNDO:
+
       if (record->event.pressed) {
+#if defined(OS_DETECTION_ENABLE)
         os_variant_t host_os = detected_host_os();
         if (host_os == OS_MACOS || host_os == OS_IOS) {
           SEND_STRING(SS_LGUI("z"));
         } else {
           SEND_STRING(SS_LCTL("z"));
         }
+#else
         SEND_STRING(SS_LCTL("z"));
+#endif
       }
-      return false; 
+      return false;
 
-    case OS_AWARE_REDO:       
+    case OS_AWARE_REDO:
+
       if (record->event.pressed) {
+#if defined(OS_DETECTION_ENABLE)
         os_variant_t host_os = detected_host_os();
         if (host_os == OS_MACOS || host_os == OS_IOS) {
           SEND_STRING(SS_LGUI(SS_LSFT("z")));
         } else {
           SEND_STRING(SS_LCTL(SS_LSFT("z")));
         }
+#else
         SEND_STRING(SS_LCTL(SS_LSFT("z")));
+#endif
       }
       return false;
 
-    case OS_AWARE_VOICE:
-      if (record->event.pressed) {
-        os_variant_t host_os = detected_host_os();
-        if (host_os == OS_MACOS || host_os == OS_IOS) {
-        tap_code16(MAC_SIRI);
-          } else if (host_os == OS_WINDOWS) {
-              SEND_STRING(SS_LGUI("h"));
-          }
-      }
-      return false;
+        case OS_AWARE_VOICE:
+            if (record->event.pressed) {
+#if defined(OS_DETECTION_ENABLE)
+                os_variant_t host_os = detected_host_os();
+                if (host_os == OS_MACOS || host_os == OS_IOS) {
+                    tap_code16(MAC_SIRI);
+                } else if (host_os == OS_WINDOWS) {
+                    SEND_STRING(SS_LGUI("h"));
+                }
+#endif
+            }
+            return false;
 
     case ALT_TAB:
       if (record->event.pressed) {
