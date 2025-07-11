0e3eba9483f08269e27e9e9822a07b27a836bfcf
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-07-11 08:20:13 -0700

    Update combos.c

--- 6JP4n/modules/combos.c
+++ 6JP4n/modules/combos.c
@@ -18,13 +18,13 @@ const uint16_t PROGMEM cmb_caps_grp[]  = { KC_N,           KC_G, COMBO_END};
 const uint16_t PROGMEM cmb_caps_rus[]  = { RU_EF,          RU_PE, COMBO_END};
 
 
-const uint16_t PROGMEM cmb_copy_grp[]  = { KC_R,           KC_L,     COMBO_END};
+const uint16_t PROGMEM cmb_copy_grp[]  = { KC_X,           KC_R,     COMBO_END};
 const uint16_t PROGMEM cmb_copy_rus[]  = { RU_CHE,        RU_YERU,   COMBO_END};
 const uint16_t PROGMEM cmb_copy_tbl[]  = { ST_MACRO_4,     ST_MACRO_1, COMBO_END};
 
 const uint16_t PROGMEM cmb_paste_grp[] = { KC_T,           KC_D,       COMBO_END};
 const uint16_t PROGMEM cmb_paste_rus[] = { RU_VE,          RU_U,       COMBO_END};
-const uint16_t PROGMEM cmb_paste_tbl[] = { ST_MACRO_2,     ST_MACRO_5, COMBO_END};
+//const uint16_t PROGMEM cmb_paste_tbl[] = { ST_MACRO_2,     ST_MACRO_5, COMBO_END};
 
 const uint16_t PROGMEM cmb_cut_grp[]   = { KC_S,           KC_W, COMBO_END};
 const uint16_t PROGMEM cmb_cut_rus[]   = { RU_A,           RU_KA, COMBO_END};
@@ -84,7 +84,7 @@ combo_t key_combos[] = {
 
     COMBO(cmb_copy_grp, OS_AWARE_COPY),  
     COMBO(cmb_copy_rus, OS_AWARE_COPY),
-    COMBO(cmb_copy_tbl, OS_AWARE_COPY),  
+    //COMBO(cmb_copy_tbl, OS_AWARE_COPY),  
 
     COMBO(cmb_paste_grp, OS_AWARE_PASTE),
     COMBO(cmb_paste_rus, OS_AWARE_PASTE),
