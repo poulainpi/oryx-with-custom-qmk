1c2b9016a52cdfcb7a2b26a6dd7d41cf6b644715
Vitaly Belousov <vitaly.belousov@gmail.com>
2025-07-21 01:10:22 -0700

    Update switch_cases.c

--- 6JP4n/modules/switch_cases.c
+++ 6JP4n/modules/switch_cases.c
@@ -1,17 +1,3 @@
-    case LANG_SWITCH_COMBO_ACTION:
-
-    if (record->event.pressed) {
-        uint8_t current_highest_layer = get_highest_layer(layer_state);
-
-        if (current_highest_layer == QMK_LAYER_ENG) {
-            layer_move(QMK_LAYER_RUS);
-            SEND_STRING(SS_LCTL(SS_LSFT("2")));
-        } else {
-            layer_move(QMK_LAYER_ENG);
-            SEND_STRING(SS_LCTL(SS_LSFT("1")));
-        }
-    }
-    return false;
 
     case OS_AWARE_COPY:
       if (record->event.pressed) {
