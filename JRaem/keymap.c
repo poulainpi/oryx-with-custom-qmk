878a4c1510ef1304b76b008e5480dbd6a2946e15
Markus Schencker <markus.schencker@schencker>
2025-08-16 23:37:54 +0200

    Fix: overrides code fix.

--- JRaem/keymap.c
+++ JRaem/keymap.c
@@ -108,7 +108,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const key_override_t dot_comma_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_COMM);
 const key_override_t quote_doublequote_override = ko_make_basic(MOD_MASK_SHIFT, DE_DQOT, DE_QUOT);
 
-const key_override_t **key_overrides = (const key_override_t *[]){
+// This globally defines all key overrides to be used
+const key_override_t *key_overrides[] = {
     &dot_comma_override,
     &quote_doublequote_override,
     NULL
