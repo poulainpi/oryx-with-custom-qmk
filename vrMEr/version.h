d13ffd1a72d296571434ef7aae587f3b0a44b0d2
Markus Schencker <markus.schencker@schencker>
2025-11-08 22:50:32 +0100

    chore: Trigger workflow - update firmware version

--- vrMEr/version.h
+++ vrMEr/version.h
@@ -3,4 +3,4 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
-#define FIRMWARE_VERSION u8"002-minimize-layers"
+#define FIRMWARE_VERSION u8"002-minimize-layers-v1"
