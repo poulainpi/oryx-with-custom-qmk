59c7c45aaa58297cb31542fc3b76c6ae0b1b27bc
Markus Schencker <markus.schencker@schencker>
2025-11-08 22:45:02 +0100

    feat(vrMEr): Add version.h for Phase 6 compilation

---
+++ vrMEr/version.h
@@ -0,0 +1,6 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+#define FIRMWARE_VERSION u8"002-minimize-layers"
