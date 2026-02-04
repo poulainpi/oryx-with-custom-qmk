32ca0bf01e984f616f3d0cc82be267dff3ad5830
msmandaisageek <mandaisageek@gmail.com>
2026-02-03 18:58:48 -0600

    Update keymap.c

--- ZZM96/keymap.c
+++ ZZM96/keymap.c
@@ -119,6 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_0
   ),
 
+
  [8] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         
     KC_TRANSPARENT, KC_EXLM,        KC_QUES,        KC_LCBR,        KC_RCBR,        KC_CIRC,                                        KC_PERC,        KC_AMPR,        KC_ASTR,        KC_UNDS,        KC_BSLS,        KC_F12,         
